11f6305c0a65cedbb6006e197b1e2af3afa01df3
WaveHack <email@wavehack.net>
2017-10-10 20:00:37 +0200

    Add some tests

---
+++ tests/ZipCodeValidatorTestConstraint.php
@@ -0,0 +1,175 @@
+<?php
+
+use Mockery as m;
+use ZipCodeValidator\Constraints\ZipCode;
+use ZipCodeValidator\Constraints\ZipCodeValidator;
+
+class ZipCodeValidatorTest extends PHPUnit_Framework_TestCase
+{
+    /** @var ZipCodeValidator */
+    protected $validator;
+
+    /** @var \Symfony\Component\Validator\Context\ExecutionContextInterface|\Mockery\Mock */
+    protected $context;
+
+    public function setUp()
+    {
+        $this->validator = new ZipCodeValidator;
+
+        $this->context = m::mock('\Symfony\Component\Validator\Context\ExecutionContext');
+
+        $this->validator->initialize($this->context);
+    }
+
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_will_validate_a_zip_code_with_iso()
+    {
+        $constraint = new ZipCode('HK');
+
+        // Test some integers
+        $this->validator->validate(999077, $constraint);
+
+        $constraint->iso = 'KE';
+        $this->validator->validate(12345, $constraint);
+
+        // Test some strings
+        $constraint->iso = 'MU';
+        $this->validator->validate('15325', $constraint);
+        $this->validator->validate('153BU123', $constraint);
+
+        $constraint->iso = 'NL';
+        $this->validator->validate('1234AB', $constraint);
+        $this->validator->validate('1234 AB', $constraint);
+
+        $constraint->iso = 'PN';
+        $this->validator->validate('PCRN 1ZZ', $constraint);
+    }
+
+    /** @test */
+    public function it_will_validate_a_zip_code_with_getter()
+    {
+        $this->context->shouldReceive('getObject')->once()
+            ->andReturn(new TestObject('VN'));
+
+        $this->validator->initialize($this->context);
+
+        $this->validator->validate(123456, new TestZipCodeConstraint);
+    }
+
+    /** @test */
+    public function it_will_not_validate_an_invalid_zip_code()
+    {
+        $value = 'invalid';
+        $constraint = new ZipCode('HK');
+
+        // Non-mocked context object due to sheer complexity. Might need a refactor
+        $translator = new \Symfony\Component\Translation\Translator('en-US');
+        $context = new \Symfony\Component\Validator\Context\ExecutionContext(
+            new \Symfony\Component\Validator\Validator\RecursiveValidator(
+                new \Symfony\Component\Validator\Context\ExecutionContextFactory($translator),
+                new \Symfony\Component\Validator\Tests\Fixtures\FakeMetadataFactory(),
+                new \Symfony\Component\Validator\ConstraintValidatorFactory()
+            ),
+            null,
+            $translator
+        );
+
+        $context->setConstraint($constraint);
+        $this->validator->initialize($context);
+
+        $this->validator->validate($value, $constraint);
+
+        $this->assertEquals(
+            $constraint->message,
+            $context->getViolations()->get(0)->getMessage()
+        );
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Validator\Exception\UnexpectedTypeException
+     */
+    public function it_will_throw_an_exception_on_invalid_constraint_class()
+    {
+        /** @var \Symfony\Component\Validator\Constraint $constraint */
+        $constraint = m::mock('\Symfony\Component\Validator\Constraint');
+
+        $this->validator->validate('dummy', $constraint);
+    }
+
+    /**
+     * Note: \BadMethodCallException is due to context being mocked. When it gets the exception, it will have processed
+     * through the validate method and into the no pattern matching block.
+     *
+     * @see it_will_not_validate_an_invalid_zip_code
+     *
+     * @test
+     * @expectedException \BadMethodCallException
+     */
+    public function it_will_throw_an_exception_on_empty_value()
+    {
+        $constraint = new ZipCode('HK');
+        $constraint->ignoreEmpty = false;
+
+        $this->validator->validate('', $constraint);
+    }
+
+    /** @test */
+    public function it_will_do_nothing_on_empty_value_if_ignore_empty_is_set()
+    {
+        $constraint = new ZipCode('HK');
+        $constraint->ignoreEmpty = true;
+
+        $this->validator->validate('', $constraint);
+    }
+
+    /** @test */
+    public function it_will_return_blank_on_empty_iso()
+    {
+        $this->context->shouldReceive('getObject')->once()
+            ->andReturn(new TestObject(null));
+
+        $this->validator->initialize($this->context);
+
+        $this->validator->validate('dummy', new TestZipCodeConstraint);
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Validator\Exception\ConstraintDefinitionException
+     */
+    public function it_will_throw_an_exception_on_invalid_iso_if_strict_mode_is_set()
+    {
+        $this->context->shouldReceive('getObject')->once()
+            ->andReturn(new TestObject('non-existing iso'));
+
+        $this->validator->initialize($this->context);
+
+        $this->validator->validate('dummy', new TestZipCodeConstraint);
+    }
+}
+
+class TestZipCodeConstraint extends ZipCode
+{
+    public $getter = 'myValidationMethod';
+}
+
+class TestObject
+{
+    protected $iso;
+
+    public function __construct($iso)
+    {
+        $this->iso = $iso;
+    }
+
+    public function myValidationMethod()
+    {
+        return $this->iso;
+    }
+}
