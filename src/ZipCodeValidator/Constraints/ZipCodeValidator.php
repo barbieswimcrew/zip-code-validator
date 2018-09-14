6d7e1c64adbd06d3bc2edc62d8bf4904bde56e14
Thomas Schulz <mail@king2500.net>
2018-09-14 19:19:32 +0200

    Added option to get ISO from object property path

--- src/ZipCodeValidator/Constraints/ZipCodeValidator.php
+++ src/ZipCodeValidator/Constraints/ZipCodeValidator.php
@@ -1,6 +1,8 @@
 <?php
 namespace ZipCodeValidator\Constraints;
 
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
@@ -228,18 +230,33 @@ class ZipCodeValidator extends ConstraintValidator
 
         if (!($iso = strtoupper($constraint->iso))) {
             // if iso code is not specified, try to fetch it via getter from the object, which is currently validated
-            $object = $this->context->getObject();
-            $getter = $constraint->getter;
 
-            if (!is_callable(array($object, $getter))) {
-                $message = 'Method "%s" used as iso code getter does not exist in class %s';
-                throw new ConstraintDefinitionException(sprintf($message, $getter, get_class($object)));
+            if ($constraint->getter || $constraint->isoPropertyPath) {
+                $object = $this->context->getRoot();
+
+                if ($object instanceof FormInterface) {
+                    $object = $object->getData();
+                }
+                if ($constraint->getter) {
+                    $getter = $constraint->getter;
+                    if (!is_callable(array($object, $getter))) {
+                        $message = 'Method "%s" used as iso code getter does not exist in class %s';
+                        throw new ConstraintDefinitionException(sprintf($message, $getter, get_class($object)));
+                    }
+                    $iso = $object->$getter();
+                }
+                if ($constraint->isoPropertyPath) {
+                    if (!class_exists(PropertyAccess::class)) {
+                        throw new \RuntimeException('The symfony/property-access component is required for "isoPropertyPath" option');
+                    }
+
+                    $propertyAccessor = PropertyAccess::createPropertyAccessor();
+                    $iso = $propertyAccessor->getValue($object, $constraint->isoPropertyPath);
+                }
             }
-
-            $iso = $object->$getter();
         }
 
-        if(empty($iso)){
+        if (empty($iso)){
             return;
         }
 
