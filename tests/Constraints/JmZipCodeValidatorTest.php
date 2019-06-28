af4db8305c16a3eee70621c38c68a576504b8a8c
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-06-28 14:26:58 +0200

    Added correct validation for Jamaica (#35)

--- tests/Constraints/JmZipCodeValidatorTest.php
+++ tests/Constraints/JmZipCodeValidatorTest.php
@@ -28,11 +28,11 @@ class JmZipCodeValidatorTest extends \PHPUnit\Framework\TestCase
     /**
      * This test verifies that all known Jamaica codes are valid.
      *
-     * @dataProvider getJamaicaZipCodes
+     * @dataProvider getValidJamaicaZipCodes
      * @test
      * @param string $zipCode
      */
-    public function testZipcodes($zipCode)
+    public function testValidZipcodes($zipCode)
     {
         $constraint = new ZipCode('JM');
 
@@ -55,7 +55,7 @@ class JmZipCodeValidatorTest extends \PHPUnit\Framework\TestCase
      *
      * @return array
      */
-    public function getJamaicaZipCodes()
+    public function getValidJamaicaZipCodes()
     {
         return [
             ['KN'],
@@ -67,4 +67,46 @@ class JmZipCodeValidatorTest extends \PHPUnit\Framework\TestCase
         ];
     }
 
+    /**
+     * This test verifies that all known Jamaica codes are valid.
+     *
+     * @dataProvider getInvalidJamaicaZipCodes
+     * @test
+     * @param string $zipCode
+     */
+    public function testInvalidZipcodes($zipCode)
+    {
+        $constraint = new ZipCode('JM');
+
+        $violation = $this->createMock(ConstraintViolationBuilderInterface::class);
+        $violation->expects($this->once())->method('setParameter')->willReturnSelf();
+
+        /** @var ExecutionContext|PHPUnit_Framework_MockObject_MockObject $contextMock */
+        $contextMock = $this->getMockBuilder(ExecutionContext::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        # be sure that buildViolation never gets called
+        $contextMock->expects($this->once())->method('buildViolation')->willReturn($violation);
+        $contextMock->setConstraint($constraint);
+
+        $this->validator->initialize($contextMock);
+        $this->validator->validate($zipCode, $constraint);
+    }
+
+    /**
+     * used postal codes
+     * from https://en.wikipedia.org/wiki/Postal_codes_in_Jamaica
+     *
+     * @return array
+     */
+    public function getInvalidJamaicaZipCodes()
+    {
+        return [
+            ['12'],
+            ['\W'],
+            ['A1'],
+            ['0z'],
+        ];
+    }
 }
