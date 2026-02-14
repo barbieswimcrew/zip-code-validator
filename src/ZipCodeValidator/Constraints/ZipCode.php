c85be5cd95fcb8d0adf667383ac309973a419357
Renaud Grand <renaud.grand@gmail.com>
2026-02-14 12:54:08 +0100

    feat: Add Symfony 8 compatibility (#66)
    * First working version
    
    * Factorize code a bit
    
    * Attempt at passing CI by fixing symfony/validator to non problematic versions
    
    * Prevent iso from being set by both legacy string and positional argument

--- src/ZipCodeValidator/Constraints/ZipCode.php
+++ src/ZipCodeValidator/Constraints/ZipCode.php
@@ -4,6 +4,7 @@ namespace ZipCodeValidator\Constraints;
 
 use Attribute;
 use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\InvalidOptionsException;
 use Symfony\Component\Validator\Exception\MissingOptionsException;
 
 /**
@@ -20,19 +21,72 @@ class ZipCode extends Constraint
     public bool $strict = true;
     public bool $caseSensitiveCheck = true;
 
-    public function __construct(mixed $options = null, ?array $groups = null, mixed $payload = null)
+    public function __construct(
+        mixed $options = null,
+        ?array $groups = null,
+        mixed $payload = null,
+        ?string $iso = null,
+        ?string $getter = null,
+        ?bool $strict = null,
+        ?bool $caseSensitiveCheck = null,
+        ?string $message = null
+    )
     {
         if (is_string($options)) {
-            $options = array(
-                'iso' => $options
+            if (null !== $iso) {
+                throw new InvalidOptionsException(
+                    'Cannot pass both positional string $options and named "iso". Use one style.',
+                    ['options', 'iso']
+                );
+            }
+
+            $options = ['iso' => $options];
+        } elseif (null === $options) {
+            $options = [];
+        } elseif (!is_array($options)) {
+            throw new InvalidOptionsException(sprintf('The options "%s" do not exist in constraint "%s".', 'options', __CLASS__), ['options']);
+        }
+
+        $resolvedOptions = [
+            'iso' => $iso,
+            'getter' => $getter,
+            'strict' => $strict,
+            'caseSensitiveCheck' => $caseSensitiveCheck,
+            'message' => $message,
+            'groups' => $groups,
+            'payload' => $payload,
+        ];
+
+        $invalidOptions = array_values(array_filter(array_keys($options), fn ($option) => !in_array($option, array_keys($resolvedOptions), true)));
+        if ([] !== $invalidOptions) {
+            throw new InvalidOptionsException(
+                sprintf('The options "%s" do not exist in constraint "%s".', implode('", "', $invalidOptions), __CLASS__),
+                $invalidOptions
             );
         }
 
-        parent::__construct($options, $groups, $payload);
+        foreach ($resolvedOptions as $option => $resolvedValue) {
+            if (null !== $resolvedValue || !array_key_exists($option, $options)) {
+                continue;
+            }
+
+            $resolvedOptions[$option] = 'groups' === $option ? (array) $options[$option] : $options[$option];
+        }
+
+        parent::__construct(null, $resolvedOptions['groups'], $resolvedOptions['payload']);
+
+        unset($resolvedOptions['groups'], $resolvedOptions['payload']);
+
+        foreach ($resolvedOptions as $option => $resolvedValue) {
+            if (null === $resolvedValue) {
+                continue;
+            }
+            
+            $this->{$option} = $resolvedValue;
+        }
 
         if (null === $this->iso && null === $this->getter) {
             throw new MissingOptionsException(sprintf('Either the option "iso" or "getter" must be given for constraint %s', __CLASS__), ['iso', 'getter']);
         }
     }
-
 }
